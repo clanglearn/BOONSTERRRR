756529c0db52d6f3e0ec766ee417c68ae05d3d30
정지현 <boonsterrrr@gmail.com>
2024-07-08 00:49:31 +0900

    이어서

--- Week1/수업때배운거정답까먹어서해보는.c
+++ Week1/수업때배운거정답까먹어서해보는.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
-   printf("Hello");
-   return 0;
-}
+// int main(){
+//    printf("Hello");
+//    return 0;
+// }
+
+// int main(){
+//    char ascii = 'A';
+//    printf("%c\n", ascii);
+//    return 0;
+// }
