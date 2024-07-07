bb50cf31255f4b148138dcf6ff0d4c970c28cd10
정지현 <boonsterrrr@gmail.com>
2024-07-08 04:43:45 +0900

    4~6구구단부분포함.+문제

--- Week1/MultiplicationTable.c
+++ Week1/MultiplicationTable.c
@@ -59,3 +59,20 @@ int main() {
 }
 
 //어머미친이게어케성공한거야 찍었는데?!!어ㅓ머머ㅓ머머ㅓㅁ//
+
+int main2() {
+	int num3 = 4;
+	do
+	{
+		int num4 = 1;
+		do
+		{
+			printf("%d * %d =%d\n", num3, num4, num3 * num4);
+			++num4;
+		} while (num4 < 10);
+		num3++;
+	} while (num3 < 7);
+	
+}
+
+//엥 각자 따로따로 하면 나오기는 하는데 어케 합치지//
