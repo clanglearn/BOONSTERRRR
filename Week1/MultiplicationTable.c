e9b0b93694ea51ad536005b359667d08586c49af
정지현 <boonsterrrr@gmail.com>
2024-07-08 02:30:08 +0900

    1~3단계 while문으로 성공

--- Week1/MultiplicationTable.c
+++ Week1/MultiplicationTable.c
@@ -20,3 +20,42 @@ int main() {
 
 
 //1차시도실패//
+
+#include <stdio.h>
+
+
+//int main() {
+//	int num1 = 1, num2 = 1;
+//	while (num1< 4 && num2 <10)
+//	{
+//		printf("%d * %d =%d\n", num1, num2, num1 * num2);
+//		++num2;
+//		
+//		while (num2 >9)
+//		{
+//			++num1;
+//		}
+//		
+//	}
+//	
+//}
+
+int main() {
+	int num1 = 1;
+	while (num1 < 4)
+	{
+		int num2 = 1;
+		while (num2 < 10)
+		{
+			printf("%d * %d =%d\n", num1, num2, num1 * num2);
+			++num2;
+
+		}
+		num1++;
+		
+
+	}
+
+}
+
+//어머미친이게어케성공한거야 찍었는데?!!어ㅓ머머ㅓ머머ㅓㅁ//
