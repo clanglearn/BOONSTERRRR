8a38117f8217a7bacb65015942becad9af561faa
정지현 <boonsterrrr@gmail.com>
2024-07-08 05:12:16 +0900

    합체!!

--- Week1/MultiplicationTable.c
+++ Week1/MultiplicationTable.c
@@ -86,3 +86,41 @@ int main() {
 }
 
 //흐흐하하ㅏㅏㅎㅎ또 성공했지뭐야???이제 합쳐야해//
+//for쓰면 ; 안쓰고 괄호로만함//
+
+int main() {
+
+	int num1 = 1;
+	while (num1 < 4)
+	{
+		int num2 = 1;
+		while (num2 < 10)
+		{
+			printf("%d * %d =%d\n", num1, num2, num1 * num2);
+			++num2;
+
+		}
+		num1++;
+
+
+	}
+
+	int num3 = 4;
+	do
+	{
+		int num4 = 1;
+		do
+		{
+			printf("%d * %d =%d\n", num3, num4, num3 * num4);
+			++num4;
+		} while (num4 < 10);
+		num3++;
+	} while (num3 < 7);
+
+	for (int num5 = 7; num5 < 10; num5++)
+		for (int num6 = 1; num6 < 10; ++num6)
+			printf("%d * %d=%d\n", num5, num6, num5 * num6);
+
+}
+
+//난 성공하고 말았어 음하하ㅏ하하ㅏ!!!!//
