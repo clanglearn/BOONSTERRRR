83a505943bac8e41a45abed8bbdd0a2814b6c6fc
정지현 <boonsterrrr@gmail.com>
2024-07-14 01:32:10 +0900

    과제1

---
+++ Week2/과제1.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int i = 5;
+	int arr[5];
+	for (int j = 0; j < i; j++) {
+		scanf("%d", &arr[j]);
+	}
+
+	int Max = arr[0];
+	for (int j = 1; j < i; j++) {
+		if (Max < arr[j]) {
+			Max = arr[j];
+
+		}
+	}
+
+	int Min = arr[0];
+	for (int j = 1; j < i; j++) {
+		if (Min > arr[j]) {
+			Min = arr[j];
+		}
+	}
+	printf("입력된 정수 중에서 최댓값 : %d\n", Max);
+	printf("입력된 정수 중에서 최솟값 : %d", Min);
+
+
+
+}
