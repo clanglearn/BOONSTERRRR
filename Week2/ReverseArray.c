a2868e661b0a53bb33d6d9d36a5a2baa2e224343
정지현 <boonsterrrr@gmail.com>
2024-07-16 01:10:18 +0900

    다른코드도 짜봤지만 실패

--- Week2/ReverseArray.c
+++ Week2/ReverseArray.c
@@ -15,3 +15,19 @@ int main() {
 }
 
 //모르겠어 그냥 질문해//
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int arr[6] = { 1,2,3,4,5,6 };
+    int p[6];
+    int i = 0;
+    while (arr[i] != '\0') {
+        p[i] = arr[5 - i];
+        i++;
+    }
+    printf("%d", p);
+}
+
+//진짜 모르겠다 뭐지 //
