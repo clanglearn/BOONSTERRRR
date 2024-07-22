6fd46e2ce90327cb9c0a9300056570669d9e9576
정지현 <boonsterrrr@gmail.com>
2024-07-23 00:00:07 +0900

    말록

---
+++ Week3/Malloc.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int num,i;
+	i = scanf("%d", &num);
+	
+	void* malloc(i);
+	for (int p = 0; p < i; p++) {
+		int j;
+		scanf("%d", &j);
+
+		printf("%d",j);
+
+	}
+	void free();
+}
+//엥 어케하는거임 이해가 안가//
